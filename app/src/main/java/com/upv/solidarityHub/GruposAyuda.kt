c5118e203ad4b8341e52670735919838d61fc1c5
bayronrojass <bayronrojas30@gmail.com>
2025-04-07 11:34:08 +0200

    Grupos Ayuda interfaz

--- app/src/main/java/com/upv/solidarityHub/GruposAyuda.kt
+++ app/src/main/java/com/upv/solidarityHub/GruposAyuda.kt
@@ -6,30 +6,20 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.upv.solidarityHub.databinding.ActivityGruposAyuda2Binding
+import com.upv.solidarityHub.databinding.ContentGruposAyuda2Binding
 import com.upv.solidarityHub.databinding.ActivityGruposAyudaBinding
 
 class GruposAyuda : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityGruposAyudaBinding
+    private lateinit var binding: ContentGruposAyuda2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityGruposAyudaBinding.inflate(layoutInflater)
+        binding = ContentGruposAyuda2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
         //setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_grupos_ayuda2)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_grupos_ayuda2)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
