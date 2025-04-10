29991c400432893f3f3f610075c4f4a449597b57
aperpal1 <aperpal1@upv.edu.es>
2025-04-10 19:58:05 +0200

    Versión primitiva de la solicitud de ayuda

--- app/src/main/java/com/upv/solidarityHub/AppNavigation.kt
+++ app/src/main/java/com/upv/solidarityHub/AppNavigation.kt
@@ -24,7 +24,7 @@ fun AppNavigation() {
         }
 
         composable("SolAyuda"){
-            context.startActivity(Intent(context, solAyuda::class.java))
+            context.startActivity(Intent(context, SolAyuda::class.java))
         }
 
         composable("MapaGenerico") {
